657e1337a1e2b822e322efc9854495ee87bbb4a0
Mike Bradbury <mike.bradbury@jugo.io>
2024-02-04 14:26:37 +0000

    small refactor to make it more extensible

--- index.js
+++ index.js
@@ -1,37 +1,65 @@
+const kvCommands = [
+  {
+    name: "get",
+    handler: (data, [key]) => data.get(key),
+  },
+  {
+    name: "set",
+    handler: (data, [key, value]) => {
+      data.set(key, value);
+      return "OK";
+    },
+  },
+  {
+    name: "del",
+    handler: (data, [key]) => {
+      const val = data.get(key);
+      data.delete(key);
+      return val;
+    },
+  },
+];
+
 class Redis {
-  constructor() {
-    this.data = new Map();
+  #data;
+  #commands;
+
+  constructor({ commands }) {
+    this.#data = new Map();
+    this.#commands = commands;
   }
 
   process(commandString) {
-    const [command, ...args] = commandString.split(" ");
-    switch (command) {
-      case "get":
-        return this.#get(...args);
-      case "set":
-        return this.#set(...args);
-      case "del":
-        return this.#del(...args);
+    const [commandName, ...args] = commandString.split(" ");
+    const command = this.#commands.find(
+      ({ name }) => name === commandName.toLowerCase()
+    );
+    if (!command) {
+      throw new Error(`Unexpected command: ${commandName}`);
     }
+    return command.handler(this.#data, args);
   }
 
   #get(key) {
-    return this.data.get(key);
+    return this.#data.get(key);
   }
 
   #set(key, value) {
-    this.data.set(key, value);
+    this.#data.set(key, value);
     return "OK";
   }
 
   #del(key) {
-    const val = this.data.get(key);
+    const val = this.#data.get(key);
     this.data.delete(key);
     return val;
   }
 }
 
-const redis = new Redis();
-["get nonexisting", 'set key "hello"', "get key", "del key"].forEach((testcase) => {
-  console.log(redis.process(testcase));
+const redis = new Redis({
+  commands: [...kvCommands],
 });
+
+["get nonexisting", 'set key "hello"', "get key", "del key"]
+  .map((testcase) => redis.process(testcase))
+  .forEach((val) => console.log(val));
